aa9f4525a38e702923ad5c937f450fba8da0e87f
timre13 <itorteli13@gmail.com>
2020-08-20 22:49:22 +0200

    Improve logger

--- Logger.cpp
+++ Logger.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
-#include <cstdlib>
+#include <iomanip>
 #include <chrono>
 
 #include "Logger.h"
 
 #define LOG_NO_COLOR
 
+static inline std::string getTime()
+{
+    namespace chr = std::chrono;
+
+    auto timeInMicroseconds{chr::duration_cast<chr::microseconds>(chr::system_clock::now().time_since_epoch()).count()};
+    std::time_t timeInSeconds{timeInMicroseconds/1000000};
+    std::tm tstruct{*localtime(&timeInSeconds)};
+    char buffer[9]{};
+
+    std::strftime(buffer, sizeof(buffer), "%H:%M:%S", &tstruct);
+
+    return std::string(buffer)+"."+std::to_string(timeInMicroseconds%1000000);
+}
+
 void Logger::fatal(const std::string &message, int exitCode)
 {
     using namespace std::chrono;
 
+    std::cerr << std::dec;
+
     #ifndef LOG_NO_COLOR
-        std::cerr << "\033[30;41m" << "[Fatal]" << "\033[0;0m: " << message << '\n';
+        std::cerr << "\033[30;41m[" << getTime() << "][Fatal]" << "\033[0;0m: " << message << '\n';
     #else
-        std::cerr << "[" <<
-                     duration_cast<milliseconds>(
-                         system_clock::now().time_since_epoch()).count() <<
-                     "][Fatal]: " << message << '\n';
+        std::cerr << "[" << getTime() << "][Fatal]: " << message << '\n';
     #endif
     std::cerr.flush();
     
@@ -27,13 +40,12 @@ void Logger::error(const std::string &message)
 {
     using namespace std::chrono;
 
+    std::cerr << std::dec;
+
     #ifndef LOG_NO_COLOR
-        std::cerr << "\033[31m" << "[Error]" << "\033[0;0m: " << message << '\n';
+        std::cerr << "\033[31m[" << getTime() << "][Error]" << "\033[0;0m: " << message << '\n';
     #else
-        std::cerr << "[" <<
-                     duration_cast<milliseconds>(
-                         system_clock::now().time_since_epoch()).count() <<
-                     "][Error]: " << message << '\n';
+        std::cerr << "[" << getTime() << "][Error]: " << message << '\n';
     #endif
     
     std::cerr.flush();
@@ -43,13 +55,12 @@ void Logger::warning(const std::string &message)
 {
     using namespace std::chrono;
 
+    std::cerr << std::dec;
+
     #ifndef LOG_NO_COLOR
-        std::cerr << "\033[33m" << "[Warning]" << "\033[0;0m: " << message << '\n';
+        std::cerr << "\033[33m[" << getTime() << "][Warning]" << "\033[0;0m: " << message << '\n';
     #else
-        std::cerr << "[" <<
-                     duration_cast<milliseconds>(
-                         system_clock::now().time_since_epoch()).count() <<
-                     "][Warning]: " << message << '\n';
+        std::cerr << "[" << getTime() << "][Warning]: " << message << '\n';
     #endif
 
     std::cerr.flush();
@@ -59,13 +70,12 @@ void Logger::info(const std::string &message)
 {
     using namespace std::chrono;
 
+    std::cout << std::dec;
+
     #ifndef LOG_NO_COLOR
-        std::cout << "\033[32m" << "[Info]" << "\033[0;0m: " << message << '\n';
+        std::cout << "\033[32m[" << getTime() << "][Info]" << "\033[0;0m: " << message << '\n';
     #else
-        std::cout << "[" <<
-                     duration_cast<milliseconds>(
-                         system_clock::now().time_since_epoch()).count() <<
-                     "][Info]: " << message << '\n';
+        std::cout << "[" << getTime() <<"][Info]: " << message << '\n';
     #endif
     
     std::cout.flush();
--- Logger.h
+++ Logger.h
@@ -2,11 +2,10 @@
 #define LOGGER_H
 
 #include <string>
-#include <chrono>
 
 namespace Logger
 {
-    void fatal(const std::string &message, int exitCode = 1);
+    void fatal(const std::string &message, int exitCode=1);
     void error(const std::string &message);
     void warning(const std::string &message);
     void info(const std::string &message);
