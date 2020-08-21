ce6033bde60dc9abdfa8a97a9aeaaa35f30b6d88
timre13 <itorteli13@gmail.com>
2020-08-21 12:42:01 +0200

    Fix compilation on 64-bit win

--- compile_for_win_x64.sh
+++ compile_for_win_x64.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-x86_64-w64-mingw32-g++ *.cpp -std=c++17 -o3 -I .h -lSDL2 -o egyptian_jumper.exe
+x86_64-w64-mingw32-c++ *.cpp -std=c++17 -I ~/SDL2_win_x86/include -L ~/SDL2_win_x86/lib -lSDL2 -lSDL2main -static-libgcc -static-libstdc++ -o egyptian_jumper.exe
+
+cp ~/SDL2_win_x86/bin/SDL2.dll .
